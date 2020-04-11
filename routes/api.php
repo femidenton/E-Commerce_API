f276c2dd4e236f5cf2ac92f0de3638ca8e5a040e
femidenton <femidenton@yahoo.co.uk>
2020-04-11 21:32:43 +0100

    Migration created

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+//when using RESTFUL api. With API no pages for create or edit. Just stores and updates
 Route::apiResource('/products', 'ProductController');
 
+
+// review url would be /products/11/reviews. To get the first'/products/' in the url, add a prefix
 Route::group(['prefix' => 'product'], function() {
     Route::apiResource('/{product}/reviews', 'ReviewController');
 });
